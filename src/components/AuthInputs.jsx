ea5b583eb6d141dbb0570cb5fcd592f212ba7b68
yurison <yu_ri_son@naver.com>
2024-02-08 13:37:07 +0900

    styled components 중첩 및 미디어 쿼리

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -29,6 +29,30 @@ const Input = styled.input`
   box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
 `;
 
+const Button = styled.button`
+  padding: 1rem 2rem;
+  font-weight: 600;
+  text-transform: uppercase;
+  border-radius: 0.25rem;
+  color: #1f2937;
+  background-color: #f0b322;
+  border-radius: 6px;
+  border: none;
+
+  &:hover {
+    background-color: #f0920e;
+  }
+`;
+
+const TextButton = styled.button`
+  color: #f0b322;
+  border: none;
+
+  &:hover {
+    color: #f0920e;
+  }
+`;
+
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState("");
   const [enteredPassword, setEnteredPassword] = useState("");
@@ -72,12 +96,8 @@ export default function AuthInputs() {
         </p>
       </ControlContainer>
       <div className="actions">
-        <button type="button" className="text-button">
-          Create a new account
-        </button>
-        <button className="button" onClick={handleLogin}>
-          Sign In
-        </button>
+        <TextButton type="button">Create a new account</TextButton>
+        <Button onClick={handleLogin}>Sign In</Button>
       </div>
     </div>
   );
