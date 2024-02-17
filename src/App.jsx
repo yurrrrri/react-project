6cb7c65f5ee36d2e607b22cf7014f2225a806921
yurison <yu_ri_son@naver.com>
2024-02-18 01:59:38 +0900

    new project init

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,7 @@
-import Player from "./components/Player.jsx";
-import TimerChallenge from "./components/TimerChallenge.jsx";
-
 function App() {
   return (
     <>
-      <Player />
-      <div id="challenges">
-        <TimerChallenge title="Easy" targetTime={1} />
-        <TimerChallenge title="Not Easy" targetTime={5} />
-        <TimerChallenge title="Getting tough" targetTime={10} />
-        <TimerChallenge title="Pros only" targetTime={15} />
-      </div>
+      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
     </>
   );
 }
