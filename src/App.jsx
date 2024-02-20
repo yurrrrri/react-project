278f16932e8864cd227e64d6fcc948efbc713f06
yurison <yu_ri_son@naver.com>
2024-02-20 16:37:44 +0900

    context API 전체 적용

--- src/App.jsx
+++ src/App.jsx
@@ -70,22 +70,20 @@ function App() {
   const ctxValue = {
     items: shoppingCart.items,
     addItemToCart: handleAddItemToCart,
+    updateItemQuantity: handleUpdateCartItemQuantity,
   };
 
   return (
     <CartContext.Provider value={ctxValue}>
-      <Header
-        cart={shoppingCart}
-        onUpdateCartItemQuantity={handleUpdateCartItemQuantity}
-      />
+      <Header />
       <Shop>
         {DUMMY_PRODUCTS.map((product) => (
           <li key={product.id}>
-            <Product {...product} onAddToCart={handleAddItemToCart} />
+            <Product {...product} />
           </li>
         ))}
       </Shop>
-      </CartContext.Provider>
+    </CartContext.Provider>
   );
 }
 
