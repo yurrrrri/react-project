f08d77b7fad64bfb33d01c29bc738bd0e9f2502a
yurison <yu_ri_son@naver.com>
2024-02-20 22:17:37 +0900

    some problems need to useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -77,10 +77,12 @@ function App() {
   return (
     <>
       <Modal open={modalIsOpen} onClose={handleStopRemovePlace}>
-        <DeleteConfirmation
-          onCancel={handleStopRemovePlace}
-          onConfirm={handleRemovePlace}
-        />
+        {modalIsOpen && (
+          <DeleteConfirmation
+            onCancel={handleStopRemovePlace}
+            onConfirm={handleRemovePlace}
+          />
+        )}
       </Modal>
 
       <header>
--- src/components/DeleteConfirmation.jsx
+++ src/components/DeleteConfirmation.jsx
@@ -1,4 +1,8 @@
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
+  setTimeout(() => {
+    onConfirm();
+  }, 3000);
+
   return (
     <div id="delete-confirmation">
       <h2>Are you sure?</h2>
