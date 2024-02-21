4f661f87f29f4999bcc1a84ab0776c70c89739f0
yurison <yu_ri_son@naver.com>
2024-02-21 20:03:08 +0900

    new project init

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,13 @@
-import logo from "../assets/quiz-logo.png";
+import { log } from '../log.js';
+import logoImg from '../assets/logo.png';
 
 export default function Header() {
+  log('<Header /> rendered', 1);
+
   return (
-    <header>
-      <img src={logo} alt="Quiz logo" />
-      <h1>ReactQuiz</h1>
+    <header id="main-header">
+      <img src={logoImg} alt="Magnifying glass analyzing a document" />
+      <h1>React - Behind The Scenes</h1>
     </header>
   );
 }
