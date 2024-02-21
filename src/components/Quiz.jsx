e72b6772b8d733e7becd471a195c42cc105bd008
yurison <yu_ri_son@naver.com>
2024-02-21 15:03:25 +0900

    cleanup

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -36,7 +36,7 @@ export default function Quiz() {
   return (
     <div id="quiz">
       <div id="question">
-        <QuestionTimer timeout={10000} onTimeout={handleSkipAnswer} />
+        <QuestionTimer key={activeQuestionIndex} timeout={10000} onTimeout={handleSkipAnswer} />
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
           {shuffledAnswers.answers.map((answer) => {
