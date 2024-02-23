d8ef28dedc0b0a6268616bd83d4259806df5dcc7
yurison <yu_ri_son@naver.com>
2024-02-23 09:10:16 +0900

    fetching error handling

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -5,18 +5,36 @@ import Places from "./Places.jsx";
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetching, setIsFetching] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [error, setError] = useState();
 
   useEffect(() => {
     async function fetchPlaces() {
       setIsFetching(true);
-      const response = await fetch("http://localhost:3000/places");
-      const resData = await response.json();
-      setAvailablePlaces(resData.places);
+
+      try {
+        const response = await fetch("http://localhost:3000/places");
+        const resData = await response.json();
+
+        if (!response.ok) {
+          throw new Error("Fialed to fetch places.");
+        }
+
+        setAvailablePlaces(resData.places);
+      } catch (error) {
+        setError({
+          message: error.message || "Could not fetch places, please try again later.",
+        });
+      }
+
       setIsFetching(false);
     }
 
     fetchPlaces();
   }, []);
+
+  if (error) {
+    return <Error title="An error occurred!" message={error.message} />;
+  }
 
   return (
     <Places
