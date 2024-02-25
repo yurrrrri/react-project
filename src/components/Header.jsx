b1a2ff0fadd908a0415bad2b3390e02a52e4a021
yurison <yu_ri_son@naver.com>
2024-02-25 22:44:17 +0900

    Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import logo from "../assets/logo.jpg";
+
+export default function Header() {
+  return (
+    <header id="main-header">
+      <div id="title">
+        <img src={logo} alt="A restaurant" />
+        <h1>ReactFood</h1>
+      </div>
+      <nav>
+        <button>Cart (0)</button>
+      </nav>
+    </header>
+  );
+}
