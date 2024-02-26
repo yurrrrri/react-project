5fe4cd7964815465fbb6e9fbb21a1a605b65d649
yurison <yu_ri_son@naver.com>
2024-02-26 16:48:00 +0900

    Error component

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -11,7 +11,11 @@ export default function Meals() {
   } = useHttp("http://localhost:3000/meals", requestConfig, []);
 
   if (isLoading) {
-    return <p>Fetching meals...</p>;
+    return <p className="center">Fetching meals...</p>;
+  }
+
+  if (error) {
+    <Error title="Failed to fetch meals" message={error} />;
   }
 
   return (
