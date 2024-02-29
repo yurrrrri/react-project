06bd775b0dec66a41b4367202695b549e367dd83
yurison <yu_ri_son@naver.com>
2024-02-29 21:42:27 +0900

    Cart, CartItem handling

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -11,9 +11,11 @@ const cartSlice = createSlice({
       const newItem = action.payload;
       const existingItem = state.items.find((item) => item.id === newItem.id);
 
+      state.totalQuantity++;
+
       if (!existingItem) {
         state.items.push({
-          itemId: newItem.id,
+          id: newItem.id,
           price: newItem.price,
           quantity: 1,
           totalPrice: newItem.price,
@@ -24,7 +26,19 @@ const cartSlice = createSlice({
         existingItem.totalPrice = existingItem.totalPrice + newItem.price;
       }
     },
-    removeItemFromCart() {},
+    removeItemFromCart(state, action) {
+      const id = action.payload;
+      const existingItem = state.items.find((item) => item.id === id);
+
+      state.totalQuantity--;
+
+      if (existingItem.quantity === 1) {
+        state.items = state.item.filter((item) => item.id !== id);
+      } else {
+        existingItem.quantity--;
+        existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
+      }
+    },
   },
 });
 
