efb49dba9e2089d91d657f2b13bd84d801996daf
yurison <yu_ri_son@naver.com>
2024-02-29 13:37:35 +0900

    refactor using Redux toolkit

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,7 +1,7 @@
-import { useSelector, useDispatch, connect } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 
+import { counterActions } from "../store/index";
 import classes from "./Counter.module.css";
-import { Component } from "react";
 
 const Counter = () => {
   const dispatch = useDispatch();
@@ -9,19 +9,19 @@ const Counter = () => {
   const show = useSelector((state) => state.showCounter);
 
   const incrementHandler = () => {
-    dispatch({ type: "increment" });
+    dispatch(counterActions.increment());
   };
 
   const increaseHandler = () => {
-    dispatch({ type: "increase", amount: 5 });
+    dispatch(counterActions.increase(5));
   };
 
   const decrementHandler = () => {
-    dispatch({ type: "decrement" });
+    dispatch(counterActions.decrement());
   };
 
   const toggleCounterHandler = () => {
-    dispatch({ type: "toggle" });
+    dispatch(counterActions.toogleCounter());
   };
 
   return (
