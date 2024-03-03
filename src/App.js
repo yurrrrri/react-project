316bee491c23ac61ef6ce1a870db077de73f5f64
yurison <yu_ri_son@naver.com>
2024-03-03 22:39:56 +0900

    second Router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import HomePage from "./pages/Home";
 
-const router = createBrowserRouter([{ path: "/", element: <HomePage /> }]);
+import HomePage from "./pages/Home";
+import ProductsPage from "./pages/Products";
+
+const router = createBrowserRouter([
+  { path: "/", element: <HomePage /> },
+  { path: "/products", element: <ProductsPage /> },
+]);
 
 function App() {
   return <RouterProvider router={router} />;
---
+++ src/pages/Products.js
@@ -0,0 +1,5 @@
+function ProductsPage() {
+  return <h1>The Products PAge</h1>;
+}
+
+export default ProductsPage;
