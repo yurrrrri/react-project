a4eaecb38d470e2b20eb1371ea21f1f9638187d2
yurison <yu_ri_son@naver.com>
2024-03-04 23:30:57 +0900

    nested routes handling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import EventDetailPage from "./pages/EventDetail";
 import NewEventPage from "./pages/NewEvent";
 import EditEventPage from "./pages/EditEvent";
 import RootLayout from "./pages/Root";
+import EventsRootLayout from "./pages/EventsRoot";
 
 const router = createBrowserRouter([
   {
@@ -13,10 +14,16 @@ const router = createBrowserRouter([
     element: <RootLayout />,
     children: [
       { index: true, element: <HomePage /> },
-      { path: "events", element: <EventsPage /> },
-      { path: "events/:eventId", element: <EventDetailPage /> },
-      { path: "events/new", element: <NewEventPage /> },
-      { path: "events/:eventId/edit", element: <EditEventPage /> },
+      {
+        path: "events",
+        element: <EventsRootLayout />,
+        children: [
+          { index: true, element: <EventsPage /> },
+          { path: ":eventId", element: <EventDetailPage /> },
+          { path: "new", element: <NewEventPage /> },
+          { path: ":eventId/edit", element: <EditEventPage /> },
+        ],
+      },
     ],
   },
 ]);
--- frontend/src/pages/EventDetail.js
+++ frontend/src/pages/EventDetail.js
@@ -1,5 +1,14 @@
+import { useParams } from "react-router-dom";
+
 function EventDetailPage() {
-  return <h1>EventDetailPage</h1>;
+  const params = useParams();
+
+  return (
+    <>
+      <h1>EventDetailPage</h1>
+      <p>Event ID: {params.eventId}</p>
+    </>
+  );
 }
 
 export default EventDetailPage;
