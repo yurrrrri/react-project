8508be3f248ef9b5f7559a458704be4484b89d9f
yurison <yu_ri_son@naver.com>
2024-03-05 13:42:13 +0900

    loader returning raw response

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,18 +1,23 @@
+import { useLoaderData } from "react-router-dom";
+
 import EventsList from "../components/EventsList";
 
 function EventsPage() {
-  return <EventsList />;
+  const data = useLoaderData();
+  const events = data.events;
+
+  return <EventsList events={events} />;
 }
 
 export default EventsPage;
 
+// 서버가 아닌 브라우저에서 실행 -> 모든 브라우저 API 사용 가능
 export async function loader() {
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
     // ...
   } else {
-    const resData = await response.json();
-    return resData.events;
+    return response;
   }
 }
