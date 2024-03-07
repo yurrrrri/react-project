e5ed32858233aa679b38caee39fb1420c119a30c
yurison <yu_ri_son@naver.com>
2024-03-08 08:44:50 +0900

    search with render props and function to create key dynamically

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import amazonImg from "./assets/amazon-river.jpg";
 import caribbeanImg from "./assets/caribbean-beach.jpg";
 import desertImg from "./assets/desert-dunes.jpg";
 import forestImg from "./assets/forest-waterfall.jpg";
+import Place from "./Place";
 
 const PLACES = [
   {
@@ -77,8 +78,12 @@ function App() {
         </Accordion>
       </section>
       <section>
-        <SearchableList items={PLACES} />
-        <SearchableList items={["item 1", "item 2"]} />
+        <SearchableList items={PLACES} itemKeyFn={(item) => item.id}>
+          {(item) => <Place item={item} />}
+        </SearchableList>
+        <SearchableList items={["item 1", "item 2"]} itemKeyFn={(item) => item}>
+          {(item) => item}
+        </SearchableList>
       </section>
     </main>
   );
--- src/components/SearchableList/SearchableList.jsx
+++ src/components/SearchableList/SearchableList.jsx
@@ -1,10 +1,22 @@
-export default function SearchableList({ items }) {
+import { useState } from "react";
+
+export default function SearchableList({ items, itemKeyFn, children }) {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const searchResults = items.filter((item) =>
+    JSON.stringify(item).toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  function handleChange(event) {
+    setSearchTerm(event.target.value);
+  }
+
   return (
     <div className="searchable-list">
-      <input type="search" placeholder="Search" />
+      <input type="search" placeholder="Search" onChange={handleChange} />
       <ul>
-        {items.map((item, index) => (
-          <li key={index}>{item.toString()}</li>
+        {searchResults.map((item) => (
+          <li key={itemKeyFn(item)}>{children(item)}</li>
         ))}
       </ul>
     </div>
