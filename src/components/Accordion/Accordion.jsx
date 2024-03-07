9154156e91810b9a253bbd1d208900eba9de2536
yurison <yu_ri_son@naver.com>
2024-03-07 15:14:57 +0900

    created components that highly utilized

--- src/components/Accordion/Accordion.jsx
+++ src/components/Accordion/Accordion.jsx
@@ -1,5 +1,8 @@
 import { createContext, useContext, useState } from "react";
+
 import AccordionItem from "./AccordionItem";
+import AccordionTitle from "./AccordionTitle";
+import AccordionContent from "./AccordionContent";
 
 const AccordionContext = createContext();
 
@@ -30,3 +33,5 @@ export default function Accordion({ children, className }) {
 }
 
 Accordion.Item = AccordionItem;
+Accordion.Title = AccordionTitle;
+Accordion.Content = AccordionContent;
