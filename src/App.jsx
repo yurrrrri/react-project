544ab6ab9359cfca67c3c90000b613b5765a2cfb
yurison <yu_ri_son@naver.com>
2024-03-07 16:07:07 +0900

    sharing component states

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,11 @@ function App() {
       <section>
         <h2>Why work with us?</h2>
         <Accordion className="accordion">
-          <Accordion.Item className="accordion-item">
-            <Accordion.Title id="experience" className="accordion-item-title">
+          <Accordion.Item id="experience" className="accordion-item">
+            <Accordion.Title className="accordion-item-title">
               We got 20 years of experience
             </Accordion.Title>
-            <Accordion.Content id="experience" className="accordion-item-content">
+            <Accordion.Content className="accordion-item-content">
               <article>
                 <p>You can&apos;t go wrong with us.</p>
                 <p>
@@ -21,11 +21,11 @@ function App() {
             </Accordion.Content>
           </Accordion.Item>
 
-          <Accordion.Item className="accordion-item">
-            <Accordion.Title id="local-guides" className="accordion-item-title">
+          <Accordion.Item id="local-guides" className="accordion-item">
+            <Accordion.Title className="accordion-item-title">
               We're working with local guides
             </Accordion.Title>
-            <Accordion.Content id="local-guides" className="accordion-item-content">
+            <Accordion.Content className="accordion-item-content">
               <article>
                 <p>We are not doing this along from our office.</p>
                 <p>
--- src/components/Accordion/AccordionItem.jsx
+++ src/components/Accordion/AccordionItem.jsx
@@ -1,3 +1,24 @@
-export default function AccordionItem({ className, children }) {
-  return <li className={className}>{children}</li>;
+import { useContext } from "react";
+import { createContext } from "react";
+
+const AccordionItemContext = createContext();
+
+export function useAccordionItemContext() {
+  const ctx = useContext(AccordionItemContext);
+
+  if (!ctx) {
+    throw new Error(
+      "AccordionItem-related components must be wrapped by <Accordion.Item>."
+    );
+  }
+
+  return ctx;
+}
+
+export default function AccordionItem({ id, className, children }) {
+  return (
+    <AccordionItemContext.Provider value={id}>
+      <li className={className}>{children}</li>
+    </AccordionItemContext.Provider>
+  );
 }
