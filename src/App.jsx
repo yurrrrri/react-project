08fb2c915f5d7f85c1572da9c4eff198d3b54c72
yurison <yu_ri_son@naver.com>
2024-03-07 13:17:11 +0900

    react query and react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import {
-  Navigate,
-  RouterProvider,
-  createBrowserRouter,
-} from "react-router-dom";
+import { Navigate, RouterProvider, createBrowserRouter } from "react-router-dom";
 import { QueryClientProvider } from "@tanstack/react-query";
 
 import Events from "./components/Events/Events.jsx";
 import EventDetails from "./components/Events/EventDetails.jsx";
 import NewEvent from "./components/Events/NewEvent.jsx";
-import EditEvent from "./components/Events/EditEvent.jsx";
+import EditEvent, {
+  loader as editEventLoader,
+  action as editEventAction,
+} from "./components/Events/EditEvent.jsx";
 import { queryClient } from "./util/http.js";
 
 const router = createBrowserRouter([
@@ -34,6 +33,8 @@ const router = createBrowserRouter([
       {
         path: "/events/:id/edit",
         element: <EditEvent />,
+        loader: editEventLoader,
+        action: editEventAction,
       },
     ],
   },
