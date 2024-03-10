08ed1bbfb68f3a9c67d5a074a41031897d0cf4ad
yurison <yu_ri_son@naver.com>
2024-03-10 18:42:46 +0900

    added prop using generic type

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,10 +1,11 @@
-function Todos() {
+const Todos: React.FC<{ items: string[] }> = (props) => {
   return (
     <ul>
-      <li>Learn React</li>
-      <li>Learn TypeScript</li>
+      {props.items.map((item) => (
+        <li key={item}>{item}</li>
+      ))}
     </ul>
   );
-}
+};
 
 export default Todos;
